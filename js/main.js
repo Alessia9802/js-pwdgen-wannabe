6f915b71eeaa1365f137b7e9bf64161e0a44f6aa
Alessia9802 <alessiamisciagna@yahoo.it>
2021-10-08 15:06:00 +0200

    Esercizio non completo

---
+++ js/main.js
@@ -0,0 +1,23 @@
+/* TRACCIA: 
+Chiedi all’utente il suo nome, 
+poi chiedi il suo cognome, 
+poi chiedi il suo colore preferito */
+
+// Infine scrivi sulla pagina nomecognomecolorepreferito21
+
+const nome = prompt("Qual è il tuo nome?");
+console.log("nome");
+
+const cognome = prompt("Qual è il tuo cognome?");
+console.log("cognome");
+
+const colore = prompt("Qual è il tuo colore preferito?");
+console.log("colore");
+
+const numero = prompt("Scrivi numero 21");
+console.log("numero");
+
+document.getElementById("Alessia").innerHTML = "nome";
+document.getElementById("Misciagna").innerHTML = "cognome";
+document.getElementById("Azzurro").innerHTML = "colore";
+document.getElementById("21").innerHTML = "numero";
