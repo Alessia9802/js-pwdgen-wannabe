713ecabb64db33ff3c54f076b318d49227e86a66
Alessia9802 <alessiamisciagna@yahoo.it>
2021-10-08 15:39:22 +0200

    Correzione codice js

--- js/main.js
+++ js/main.js
@@ -6,16 +6,16 @@ poi chiedi il suo colore preferito */
 // Infine scrivi sulla pagina nomecognomecolorepreferito21
 
 const nome = prompt("Qual è il tuo nome?");
-console.log("nome");
+console.log(nome);
 
 const cognome = prompt("Qual è il tuo cognome?");
-console.log("cognome");
+console.log(cognome);
 
 const colore = prompt("Qual è il tuo colore preferito?");
-console.log("colore");
+console.log(colore);
 
 const numero = 21;
-console.log("numero");
+console.log(numero);
 
 document.getElementById("Password").innerHTML =
-  "nome" + "cognome" + "colore" + "numero";
+  nome + cognome + colore + numero;
